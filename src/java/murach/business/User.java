e98867f38717ddc615f294643bcff32524b6ea69
hungc <hungc@MANHUNG>
2025-11-25 15:56:47 +0700

    thay dổi mới

--- src/java/murach/business/User.java
+++ src/java/murach/business/User.java
@@ -7,21 +7,13 @@ public class User implements Serializable {
     private String firstName;
     private String lastName;
     private String email;
-    private String dob;
-    private String hear;
-    private String receiveCDs;
-    private String receiveEmails;
-    private String contactMethod;
+    
 
     public User() {
         firstName = "";
         lastName = "";
         email = "";
-        dob = "";
-        hear = "";
-        receiveCDs = "";
-        receiveEmails = "";
-        contactMethod = "";
+      
     }
 
     public User(String firstName, String lastName, String email, 
@@ -30,11 +22,7 @@ public class User implements Serializable {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
-        this.dob = dob;
-        this.hear = hear;
-        this.receiveCDs = receiveCDs;
-        this.receiveEmails = receiveEmails;
-        this.contactMethod = contactMethod;
+       
     }
 
     public String getFirstName() { return firstName; }
@@ -46,18 +34,5 @@ public class User implements Serializable {
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
 
-    public String getDob() { return dob; }
-    public void setDob(String dob) { this.dob = dob; }
-
-    public String getHear() { return hear; }
-    public void setHear(String hear) { this.hear = hear; }
-
-    public String getReceiveCDs() { return receiveCDs; }
-    public void setReceiveCDs(String receiveCDs) { this.receiveCDs = receiveCDs; }
-
-    public String getReceiveEmails() { return receiveEmails; }
-    public void setReceiveEmails(String receiveEmails) { this.receiveEmails = receiveEmails; }
-
-    public String getContactMethod() { return contactMethod; }
-    public void setContactMethod(String contactMethod) { this.contactMethod = contactMethod; }
+   
 }
